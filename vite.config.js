5e6bf610dbf37aa9dca5fa0b166e3d978895c9f4
Megh Prajapati <145116897+Meghhhhh@users.noreply.github.com>
2025-03-09 11:00:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// Load environment variables from .env file
-config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
